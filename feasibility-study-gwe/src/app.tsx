ee5bf3c24309f621a1a164c5f1aa882b4d94452a
anstss <anastasiya31017@gmail.com>
2021-11-27 20:22:41 +0200

    add navbar, add menu

--- feasibility-study-gwe/src/app.tsx
+++ feasibility-study-gwe/src/app.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./components/navbar/navbar";
+import "./shared/styles.scss";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ feasibility-study-gwe/src/components/navbar/navbar.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light bg-primary fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">
+          ТЕО
+        </a>
+        <button
+          className="navbar-toggler text-white"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+        >
+          <i className="bi bi-list fs-2"></i>
+        </button>
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex={-1}
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              ТЕО
+            </h5>
+            <button
+              type="button"
+              className="btn-close text-reset"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Аналіз досягнутих обсягів видобутку води
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
