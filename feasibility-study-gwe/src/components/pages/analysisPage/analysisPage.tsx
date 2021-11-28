8c6034c3239d5bc6b962cf8809ff83106e8ef10d
anstss <anastasiya31017@gmail.com>
2021-11-28 20:58:01 +0200

    add delete-modal

--- feasibility-study-gwe/src/components/pages/analysisPage/analysisPage.tsx
+++ feasibility-study-gwe/src/components/pages/analysisPage/analysisPage.tsx
@@ -138,8 +138,9 @@ const AnalysisPage = () => {
         <button
           type="button"
           className="btn btn-danger ms-2 mb-2"
-          onClick={handleDeleteData}
           disabled={!annualWaterWithdrawalData.length}
+          data-bs-toggle="modal"
+          data-bs-target="#deleteModal"
         >
           Видалити дані
         </button>
@@ -226,6 +227,47 @@ const AnalysisPage = () => {
           </div>
         </React.Fragment>
       ) : null}
+      <div
+        className="modal fade"
+        id="deleteModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Видалення
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">Видалити данні?</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Ні
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={handleDeleteData}
+                data-bs-dismiss="modal"
+              >
+                Так
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
