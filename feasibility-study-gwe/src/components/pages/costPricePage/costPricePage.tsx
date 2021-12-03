81b9aec65ff1b3920401979f439f4fe7c36c2e15
anstss <anastasiya31017@gmail.com>
2021-12-03 18:23:49 +0200

    feat: add calculating for costPricePage

--- feasibility-study-gwe/src/components/pages/costPricePage/costPricePage.tsx
+++ feasibility-study-gwe/src/components/pages/costPricePage/costPricePage.tsx
@@ -10,12 +10,19 @@ import { useStore } from "react-redux";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux-hooks";
 import { costPriceSlice } from "../../../store/reducers/costPriceSlice";
 import DeleteModal from "../../delete-modal/deleteModal";
+import IDepreciationCharges from "../../../Types/IDepreciationCharges";
 
 const CostPricePageSchema = Yup.object().shape({
   costPrice: Yup.number().typeError("Введіть число"),
   rentRateGWUse: Yup.number().typeError("Введіть число"),
   coefficientRentRateGWUse: Yup.number().typeError("Введіть число"),
   rentRateSubsoilUse: Yup.number().typeError("Введіть число"),
+  basicSalary: Yup.number().typeError("Введіть число"),
+  electricityCosts: Yup.number().typeError("Введіть число"),
+  materialsAndSpareParts: Yup.number().typeError("Введіть число"),
+  maintenance: Yup.number().typeError("Введіть число"),
+  laboratoryWorks: Yup.number().typeError("Введіть число"),
+  otherExpenses: Yup.number().typeError("Введіть число"),
 });
 
 const CostPricePage = () => {
@@ -25,6 +32,12 @@ const CostPricePage = () => {
       rentRateGWUse: "",
       coefficientRentRateGWUse: "",
       rentRateSubsoilUse: "",
+      basicSalary: "",
+      electricityCosts: "",
+      materialsAndSpareParts: "",
+      maintenance: "",
+      laboratoryWorks: "",
+      otherExpenses: "",
     },
     validationSchema: CostPricePageSchema,
     onSubmit: (values) => {
@@ -32,13 +45,23 @@ const CostPricePage = () => {
     },
   });
 
-  const handleCancel = () => {
+  const setInitialFormikValues = () => {
     formik.setValues({
       costPrice: "",
       rentRateGWUse: "",
       coefficientRentRateGWUse: "",
       rentRateSubsoilUse: "",
+      basicSalary: "",
+      electricityCosts: "",
+      materialsAndSpareParts: "",
+      maintenance: "",
+      laboratoryWorks: "",
+      otherExpenses: "",
     });
+  };
+
+  const handleCancel = () => {
+    setInitialFormikValues();
   };
 
   const dispatch = useAppDispatch();
@@ -51,9 +74,48 @@ const CostPricePage = () => {
     rentRateGWUse,
     coefficientRentRateGWUse,
     rentRateSubsoilUse,
+    basicSalary,
+    conditionalFixedCosts,
+    electricityCosts,
+    materialsAndSpareParts,
+    maintenance,
+    laboratoryWorks,
+    otherExpenses,
+    rentGWUse,
+    rentSubsoilUse,
+    variableCosts,
+    operatingCosts,
+    operatingCostsPerOnePW,
+    costPerOneSW,
   } = useAppSelector((state) => state.costPriceReducer);
 
+  const { depreciationCharges, fixedAssetsCharges } = useAppSelector(
+    (state) => state.depreciationChargesCalculationReducer
+  );
+  const [
+    fixedAssetsAndExpensesDepreciation,
+    setFixedAssetsAndExpensesDepreciation,
+  ] = useState<number>(0);
   const { setCostPriceData, clearCostPriceData } = costPriceSlice.actions;
+
+  const calculateFixedAssetsAndExpensesDepreciation = (): number => {
+    const totalDepreciationChargesKeys = Object.keys(depreciationCharges);
+    let totalDepreciationCharges = 0;
+    totalDepreciationChargesKeys.forEach((key) => {
+      totalDepreciationCharges +=
+        depreciationCharges[key as keyof IDepreciationCharges];
+    });
+    const fixedAssetsAndExpensesDepreciation = +(
+      totalDepreciationCharges + fixedAssetsCharges
+    ).toFixed(2);
+    return fixedAssetsAndExpensesDepreciation;
+  };
+
+  useEffect(() => {
+    setFixedAssetsAndExpensesDepreciation(
+      calculateFixedAssetsAndExpensesDepreciation()
+    );
+  }, [depreciationCharges, fixedAssetsCharges]);
 
   useEffect(() => {
     if (costPrice) {
@@ -69,7 +131,36 @@ const CostPricePage = () => {
     if (rentRateSubsoilUse) {
       formik.values.rentRateSubsoilUse = rentRateSubsoilUse.toString();
     }
-  }, [costPrice, rentRateGWUse, coefficientRentRateGWUse, rentRateSubsoilUse]);
+    if (basicSalary) {
+      formik.values.basicSalary = basicSalary.toString();
+    }
+    if (electricityCosts) {
+      formik.values.electricityCosts = electricityCosts.toString();
+    }
+    if (materialsAndSpareParts) {
+      formik.values.materialsAndSpareParts = materialsAndSpareParts.toString();
+    }
+    if (maintenance) {
+      formik.values.maintenance = maintenance.toString();
+    }
+    if (laboratoryWorks) {
+      formik.values.laboratoryWorks = laboratoryWorks.toString();
+    }
+    if (otherExpenses) {
+      formik.values.otherExpenses = otherExpenses.toString();
+    }
+  }, [
+    costPrice,
+    rentRateGWUse,
+    coefficientRentRateGWUse,
+    rentRateSubsoilUse,
+    basicSalary,
+    electricityCosts,
+    materialsAndSpareParts,
+    maintenance,
+    laboratoryWorks,
+    otherExpenses,
+  ]);
 
   useEffect(() => {
     const {
@@ -77,12 +168,24 @@ const CostPricePage = () => {
       rentRateGWUse,
       rentRateSubsoilUse,
       coefficientRentRateGWUse,
+      basicSalary,
+      electricityCosts,
+      materialsAndSpareParts,
+      maintenance,
+      laboratoryWorks,
+      otherExpenses,
     } = formik.values;
     if (
       costPrice ||
       rentRateGWUse ||
       rentRateSubsoilUse ||
-      coefficientRentRateGWUse
+      coefficientRentRateGWUse ||
+      basicSalary ||
+      electricityCosts ||
+      materialsAndSpareParts ||
+      maintenance ||
+      laboratoryWorks ||
+      otherExpenses
     ) {
       setCancelIsDisabled(false);
     } else {
@@ -103,6 +206,12 @@ const CostPricePage = () => {
     formik.values.rentRateGWUse,
     formik.values.coefficientRentRateGWUse,
     formik.values.rentRateSubsoilUse,
+    formik.values.basicSalary,
+    formik.values.electricityCosts,
+    formik.values.materialsAndSpareParts,
+    formik.values.maintenance,
+    formik.values.laboratoryWorks,
+    formik.values.otherExpenses,
   ]);
 
   useEffect(() => {
@@ -110,13 +219,45 @@ const CostPricePage = () => {
       costPrice ||
       rentRateGWUse ||
       coefficientRentRateGWUse ||
-      rentRateGWUse
+      rentRateGWUse ||
+      basicSalary ||
+      electricityCosts ||
+      materialsAndSpareParts ||
+      maintenance ||
+      laboratoryWorks ||
+      otherExpenses ||
+      variableCosts ||
+      operatingCosts ||
+      operatingCostsPerOnePW ||
+      costPerOneSW
     ) {
       setDeleteIsDisabled(false);
     } else {
       setDeleteIsDisabled(true);
     }
-  }, [costPrice, rentRateGWUse, coefficientRentRateGWUse, rentRateSubsoilUse]);
+  }, [
+    costPrice,
+    rentRateGWUse,
+    coefficientRentRateGWUse,
+    rentRateSubsoilUse,
+    basicSalary,
+    electricityCosts,
+    materialsAndSpareParts,
+    maintenance,
+    laboratoryWorks,
+    otherExpenses,
+    variableCosts,
+    operatingCosts,
+    operatingCostsPerOnePW,
+    costPerOneSW,
+  ]);
+
+  const {
+    averageAnnualProjectedWaterProduction,
+    averageAnnualProjectedWaterProductionWithLosses,
+  } = useAppSelector(
+    (state) => state.analysisWaterProductionVolumesReducer.statistic
+  );
 
   const handleCalculating = () => {
     const {
@@ -124,13 +265,59 @@ const CostPricePage = () => {
       rentRateGWUse,
       rentRateSubsoilUse,
       coefficientRentRateGWUse,
+      basicSalary,
+      electricityCosts,
+      materialsAndSpareParts,
+      maintenance,
+      laboratoryWorks,
+      otherExpenses,
     } = formik.values;
+    const conditionalFixedCosts = +(
+      +basicSalary + fixedAssetsAndExpensesDepreciation
+    ).toFixed(2);
+    const rentGWUse = +(
+      ((averageAnnualProjectedWaterProduction * +rentRateGWUse) / 100) *
+      +coefficientRentRateGWUse
+    ).toFixed(2);
+    const rentSubsoilUse = +(
+      ((averageAnnualProjectedWaterProductionWithLosses * +costPrice) / 100) *
+      +rentRateSubsoilUse
+    ).toFixed(2);
+    const variableCosts = +(
+      +electricityCosts +
+      +materialsAndSpareParts +
+      +maintenance +
+      +laboratoryWorks +
+      +otherExpenses +
+      rentGWUse +
+      rentSubsoilUse
+    ).toFixed(2);
+    const operatingCosts = +(conditionalFixedCosts + variableCosts).toFixed(2);
+    const operatingCostsPerOnePW = +(
+      operatingCosts / averageAnnualProjectedWaterProduction
+    ).toFixed(2);
+    const costPerOneSW = +(
+      operatingCosts / averageAnnualProjectedWaterProductionWithLosses
+    ).toFixed(2);
     dispatch(
       setCostPriceData({
-        costPrice: +costPrice,
-        rentRateGWUse: +rentRateGWUse,
-        rentRateSubsoilUse: +rentRateSubsoilUse,
-        coefficientRentRateGWUse: +coefficientRentRateGWUse,
+        costPrice: +(+costPrice).toFixed(2),
+        rentRateGWUse: +(+rentRateGWUse).toFixed(2),
+        rentRateSubsoilUse: +(+rentRateSubsoilUse).toFixed(2),
+        coefficientRentRateGWUse: +(+coefficientRentRateGWUse).toFixed(2),
+        basicSalary: +(+basicSalary).toFixed(2),
+        conditionalFixedCosts,
+        electricityCosts: +(+electricityCosts).toFixed(2),
+        materialsAndSpareParts: +(+materialsAndSpareParts).toFixed(2),
+        maintenance: +(+maintenance).toFixed(2),
+        laboratoryWorks: +(+laboratoryWorks).toFixed(2),
+        otherExpenses: +(+otherExpenses).toFixed(2),
+        rentGWUse,
+        rentSubsoilUse,
+        variableCosts,
+        operatingCosts,
+        operatingCostsPerOnePW,
+        costPerOneSW,
       })
     );
     saveCostPriceDataToLocalStorage();
@@ -146,12 +333,7 @@ const CostPricePage = () => {
   const handleDeleteData = () => {
     localStorage.removeItem(LOCAL_STORAGE_KEY_COST_PRICE);
     dispatch(clearCostPriceData());
-    formik.setValues({
-      costPrice: "",
-      rentRateGWUse: "",
-      coefficientRentRateGWUse: "",
-      rentRateSubsoilUse: "",
-    });
+    setInitialFormikValues();
   };
 
   return (
@@ -219,6 +401,215 @@ const CostPricePage = () => {
           {formik.errors.rentRateSubsoilUse}
         </div>
       )}
+      <div className="table-responsive mt-3">
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col">№</th>
+              <th scope="col">Назва витрат</th>
+              <th scope="col">Одиниці виміру</th>
+              <th scope="col">На прогноз</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th colSpan={4} className="text-center">
+                Умовно-постійні витрати
+              </th>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>
+                Основна зарплата з нарахувань персоналу, зайнятого у видобутку
+                та водопостачанні
+              </td>
+              <td>тис.грн</td>
+              <td>
+                <input
+                  name={"basicSalary"}
+                  type="text"
+                  className="form-control"
+                  placeholder={"Введіть суму витрат"}
+                  value={formik.values.basicSalary}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.basicSalary && (
+                  <div className="invalid-feedback d-block">
+                    {formik.errors.basicSalary}
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>
+                Амортизація основних засобів та витрат на облаштування родовища
+              </td>
+              <td>тис.грн</td>
+              <td>{fixedAssetsAndExpensesDepreciation}</td>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Всього умовно-постійних</th>
+              <th>тис.грн</th>
+              <th>{conditionalFixedCosts}</th>
+            </tr>
+            <tr>
+              <th colSpan={4} className="text-center">
+                Змінні витрати
+              </th>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Плата за електроенергію</td>
+              <td>тис.грн</td>
+              <td>
+                <input
+                  name={"electricityCosts"}
+                  type="text"
+                  className="form-control"
+                  placeholder={"Введіть суму витрат"}
+                  value={formik.values.electricityCosts}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.electricityCosts && (
+                  <div className="invalid-feedback d-block">
+                    {formik.errors.electricityCosts}
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Матеріали та запчастини</td>
+              <td>тис.грн</td>
+              <td>
+                <input
+                  name={"materialsAndSpareParts"}
+                  type="text"
+                  className="form-control"
+                  placeholder={"Введіть суму витрат"}
+                  value={formik.values.materialsAndSpareParts}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.materialsAndSpareParts && (
+                  <div className="invalid-feedback d-block">
+                    {formik.errors.materialsAndSpareParts}
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">5</th>
+              <td>Поточний ремонт</td>
+              <td>тис.грн</td>
+              <td>
+                <input
+                  name={"maintenance"}
+                  type="text"
+                  className="form-control"
+                  placeholder={"Введіть суму витрат"}
+                  value={formik.values.maintenance}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.maintenance && (
+                  <div className="invalid-feedback d-block">
+                    {formik.errors.maintenance}
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">6</th>
+              <td>Лабораторні роботи</td>
+              <td>тис.грн</td>
+              <td>
+                <input
+                  name={"laboratoryWorks"}
+                  type="text"
+                  className="form-control"
+                  placeholder={"Введіть суму витрат"}
+                  value={formik.values.laboratoryWorks}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.laboratoryWorks && (
+                  <div className="invalid-feedback d-block">
+                    {formik.errors.laboratoryWorks}
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">7</th>
+              <td>Інші витрати</td>
+              <td>тис.грн</td>
+              <td>
+                <input
+                  name={"otherExpenses"}
+                  type="text"
+                  className="form-control"
+                  placeholder={"Введіть суму витрат"}
+                  value={formik.values.otherExpenses}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.otherExpenses && (
+                  <div className="invalid-feedback d-block">
+                    {formik.errors.otherExpenses}
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">8</th>
+              <td>Рентна плата за спецводокористування</td>
+              <td>тис.грн</td>
+              <td>{rentGWUse}</td>
+            </tr>
+            <tr>
+              <th scope="row">9</th>
+              <td>Рентна плата за надра</td>
+              <td>тис.грн</td>
+              <td>{rentSubsoilUse}</td>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Всього змінні витрати</th>
+              <th>тис.грн</th>
+              <th>{variableCosts}</th>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Разом експлуатаційні витрати</th>
+              <th>тис.грн</th>
+              <th>{operatingCosts}</th>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Річний обсяг видобутої води</th>
+              <th>тис.грн</th>
+              <th>{averageAnnualProjectedWaterProduction}</th>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Експлуатаційні витрати на 1 м³ видобутої води</th>
+              <th>грн</th>
+              <th>{operatingCostsPerOnePW}</th>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Річний обсяг використаної води</th>
+              <th>тис.грн</th>
+              <th>{averageAnnualProjectedWaterProductionWithLosses}</th>
+            </tr>
+            <tr>
+              <th scope="row"></th>
+              <th>Собівартість 1 м³ реалізованої води</th>
+              <th>грн</th>
+              <th>{costPerOneSW}</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <ButtonGroup
         handleCancel={handleCancel}
         cancelIsDisabled={cancelIsDisabled}
--- feasibility-study-gwe/src/store/reducers/costPriceSlice.ts
+++ feasibility-study-gwe/src/store/reducers/costPriceSlice.ts
@@ -5,6 +5,19 @@ interface costPriceState {
   rentRateGWUse: number;
   coefficientRentRateGWUse: number;
   rentRateSubsoilUse: number;
+  basicSalary: number;
+  conditionalFixedCosts: number;
+  electricityCosts: number;
+  materialsAndSpareParts: number;
+  maintenance: number;
+  laboratoryWorks: number;
+  otherExpenses: number;
+  rentGWUse: number;
+  rentSubsoilUse: number;
+  variableCosts: number;
+  operatingCosts: number;
+  operatingCostsPerOnePW: number;
+  costPerOneSW: number;
 }
 
 const initialState: costPriceState = {
@@ -12,6 +25,19 @@ const initialState: costPriceState = {
   rentRateGWUse: 0,
   coefficientRentRateGWUse: 0,
   rentRateSubsoilUse: 0,
+  basicSalary: 0,
+  conditionalFixedCosts: 0,
+  electricityCosts: 0,
+  materialsAndSpareParts: 0,
+  maintenance: 0,
+  laboratoryWorks: 0,
+  otherExpenses: 0,
+  rentGWUse: 0,
+  rentSubsoilUse: 0,
+  variableCosts: 0,
+  operatingCosts: 0,
+  operatingCostsPerOnePW: 0,
+  costPerOneSW: 0,
 };
 
 export const costPriceSlice = createSlice({
@@ -24,11 +50,37 @@ export const costPriceSlice = createSlice({
         rentRateGWUse,
         coefficientRentRateGWUse,
         rentRateSubsoilUse,
+        basicSalary,
+        conditionalFixedCosts,
+        electricityCosts,
+        materialsAndSpareParts,
+        maintenance,
+        laboratoryWorks,
+        otherExpenses,
+        rentGWUse,
+        rentSubsoilUse,
+        variableCosts,
+        operatingCosts,
+        operatingCostsPerOnePW,
+        costPerOneSW,
       } = action.payload;
       state.costPrice = costPrice;
       state.rentRateGWUse = rentRateGWUse;
       state.coefficientRentRateGWUse = coefficientRentRateGWUse;
       state.rentRateSubsoilUse = rentRateSubsoilUse;
+      state.basicSalary = basicSalary;
+      state.conditionalFixedCosts = conditionalFixedCosts;
+      state.electricityCosts = electricityCosts;
+      state.materialsAndSpareParts = materialsAndSpareParts;
+      state.maintenance = maintenance;
+      state.laboratoryWorks = laboratoryWorks;
+      state.otherExpenses = otherExpenses;
+      state.rentGWUse = rentGWUse;
+      state.rentSubsoilUse = rentSubsoilUse;
+      state.variableCosts = variableCosts;
+      state.operatingCosts = operatingCosts;
+      state.operatingCostsPerOnePW = operatingCostsPerOnePW;
+      state.costPerOneSW = costPerOneSW;
     },
     clearCostPriceData(state) {
       const {
@@ -36,11 +88,37 @@ export const costPriceSlice = createSlice({
         rentRateGWUse,
         coefficientRentRateGWUse,
         rentRateSubsoilUse,
+        basicSalary,
+        conditionalFixedCosts,
+        electricityCosts,
+        materialsAndSpareParts,
+        maintenance,
+        laboratoryWorks,
+        otherExpenses,
+        rentGWUse,
+        rentSubsoilUse,
+        variableCosts,
+        operatingCosts,
+        operatingCostsPerOnePW,
+        costPerOneSW,
       } = initialState;
       state.costPrice = costPrice;
       state.rentRateGWUse = rentRateGWUse;
       state.coefficientRentRateGWUse = coefficientRentRateGWUse;
       state.rentRateSubsoilUse = rentRateSubsoilUse;
+      state.basicSalary = basicSalary;
+      state.conditionalFixedCosts = conditionalFixedCosts;
+      state.electricityCosts = electricityCosts;
+      state.materialsAndSpareParts = materialsAndSpareParts;
+      state.maintenance = maintenance;
+      state.laboratoryWorks = laboratoryWorks;
+      state.otherExpenses = otherExpenses;
+      state.rentGWUse = rentGWUse;
+      state.rentSubsoilUse = rentSubsoilUse;
+      state.variableCosts = variableCosts;
+      state.operatingCosts = operatingCosts;
+      state.operatingCostsPerOnePW = operatingCostsPerOnePW;
+      state.costPerOneSW = costPerOneSW;
     },
   },
 });
